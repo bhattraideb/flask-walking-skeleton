960df137dd958f10fdff2df6d613a9e327832ae8
BhattraiDeb <bhatrai.deb@gmail.com>
2025-05-23 17:54:38 +0530

    Add ECS task policy

--- terraform/modules/aws_ecs/iam_policy.tf
+++ terraform/modules/aws_ecs/iam_policy.tf
@@ -5,8 +5,7 @@ resource "aws_iam_role" "ecs_task_execution_role" {
     Version = "2012-10-17"
     Statement = [
       {
-        # Action = "sts:AssumeRole"
-        Action = "ecr:GetAuthorizationToken"
+        Action = "sts:AssumeRole"
         Effect = "Allow"
         Principal = {
           Service = "ecs-tasks.amazonaws.com"
@@ -22,24 +21,50 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 
 
 # IAM Role for ECS Tasks (if your application needs to interact with other AWS services)
-resource "aws_iam_role" "ecs_task_role" {
-  name_prefix = "ecs-task-role-"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        # Action = "sts:AssumeRole"
-        Action = "ecr:GetAuthorizationToken"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
+# resource "aws_iam_role" "ecs_task_role" {
+#   name_prefix = "ecs-task-role-"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole"
+#         # Action = "ecr:GetAuthorizationToken"
+#         Effect = "Allow"
+#         Principal = {
+#           Service = "ecs-tasks.amazonaws.com"
+#         }
+#       },
+#     ]
+#   })
+#
+#   tags = {
+#     Name = "ecs-task"
+#   }
+# }
+
+resource "aws_iam_role_policy" "ecs_execution_policy" {
+          name = "ecs-task-execution-policy"
+          role = aws_iam_role.ecs_task_execution_role.id
+
+          policy = jsonencode({
+            Version = "2012-10-17"
+            Statement = [
+              {
+                Action = [
+                  "ecr:GetAuthorizationToken",
+                  "ecr:BatchCheckLayerAvailability",
+                  "ecr:DownloadLayerPart",
+                  "ecr:GetDownloadURLForLayer",
+                  "ecr:GetRepositoryPolicy",
+                  "ecr:DescribeRepositories",
+                  "ecr:DescribeRegistry",
+                  "ecr:ListImages",
+                  "ecr:DescribeImage",
+                  "ecr:BatchCheckLayerAvailability",
+                ],
+                Effect = "Allow",
+                Resource = "*"
+              }
+            ]
+          })
         }
-      },
-    ]
-  })
-
-  tags = {
-    Name = "ecs-task"
-  }
-}
-
