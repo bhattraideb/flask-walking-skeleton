790d56d7fbf7c1272ed3b6ab5daafc22a0910569
BhattraiDeb <bhatrai.deb@gmail.com>
2025-05-23 20:59:54 +0530

    Update image

--- terraform/modules/aws_ecs/iam_policy.tf
+++ terraform/modules/aws_ecs/iam_policy.tf
@@ -19,50 +19,52 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   }
 }
 
-
-# IAM Role for ECS Tasks (if your application needs to interact with other AWS services)
-resource "aws_iam_role" "ecs_task_role" {
-  name_prefix = "ecs-task-role-"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      },
-    ]
-  })
-
-  tags = {
-    Name = "ecs-task"
-  }
+# Attach the required policy for ECS task execution (pulling images, logging)
+resource "aws_iam_policy_attachment" "ecs_task_execution_policy" {
+  name = "ecs-task-execution-policy-"
+  policy_arn  = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  roles       = [aws_iam_role.ecs_task_execution_role.name]
 }
 
-resource "aws_iam_role_policy" "ecs_execution_policy" {
-          name = "ecs-task-execution-policy"
-          role = aws_iam_role.ecs_task_execution_role.id
 
-          policy = jsonencode({
-            Version = "2012-10-17"
-            Statement = [
-              {
-                Action = [
-                  "ecr:GetAuthorizationToken",
-                  "ecr:BatchCheckLayerAvailability",
-                  "ecr:GetDownloadURLForLayer",
-                  "ecr:GetRepositoryPolicy",
-                  "ecr:DescribeRepositories",
-                  "ecr:DescribeRegistry",
-                  "ecr:ListImages",
-                  "ecr:BatchCheckLayerAvailability",
-                  "logs:CreateLogStream"
-                ],
-                Effect = "Allow",
-                Resource = "*"     #"arn:aws:ecr:us-east-1:645662440846:repository/cloud-native-flask-api"
-              }
-            ]
-          })
-        }
+# IAM Role for ECS Tasks (if your application needs to interact with other AWS services)
+# resource "aws_iam_role" "ecs_task_role" {
+#   name_prefix = "ecs-task-role-"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole"
+#         Effect = "Allow"
+#         Principal = {
+#           Service = "ecs-tasks.amazonaws.com"
+#         }
+#       },
+#     ]
+#   })
+#
+#   tags = {
+#     Name = "ecs-task"
+#   }
+# }
+
+# resource "aws_iam_role_policy" "ecs_execution_policy" {
+#           name = "ecs-task-execution-policy"
+#           role = aws_iam_role.ecs_task_execution_role.id
+#
+#           policy = jsonencode({
+#             Version = "2012-10-17"
+#             Statement = [
+#               {
+#                 Action = [
+#                   "ecr:GetAuthorizationToken",
+#                   "ecr:BatchCheckLayerAvailability",
+#                   "ecr:GetDownloadURLForLayer",
+#                   "ecr:*"
+#                 ],
+#                 Effect = "Allow",
+#                 Resource = "*"     #"arn:aws:ecr:us-east-1:645662440846:repository/cloud-native-flask-api"
+#               }
+#             ]
+#           })
+#         }
