bdb36db4d0060ccd59971cc5b887c0157f319b9e
BhattraiDeb <bhatrai.deb@gmail.com>
2025-05-23 17:56:40 +0530

    Add ECS task role

--- terraform/modules/aws_ecs/iam_policy.tf
+++ terraform/modules/aws_ecs/iam_policy.tf
@@ -21,26 +21,25 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 
 
 # IAM Role for ECS Tasks (if your application needs to interact with other AWS services)
-# resource "aws_iam_role" "ecs_task_role" {
-#   name_prefix = "ecs-task-role-"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         # Action = "ecr:GetAuthorizationToken"
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "ecs-tasks.amazonaws.com"
-#         }
-#       },
-#     ]
-#   })
-#
-#   tags = {
-#     Name = "ecs-task"
-#   }
-# }
+resource "aws_iam_role" "ecs_task_role" {
+  name_prefix = "ecs-task-role-"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name = "ecs-task"
+  }
+}
 
 resource "aws_iam_role_policy" "ecs_execution_policy" {
           name = "ecs-task-execution-policy"
